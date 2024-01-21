0b25770622f6c7a306e704bf928852acc61103c8
MjChen120 <156042164+MjChen120@users.noreply.github.com>
2024-01-21 01:25:58 -0500

    downloaded data

---
+++ scripts/01-download_data.R
@@ -0,0 +1,50 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from the library 'opendatatoronto'
+# Author: Mingjia Chen
+# Date: 20 January 2024 
+# Contact: mingjia.chen@mail.utoronto.ca 
+# License: MIT
+# Pre-requisites: none
+# Datasets:
+  # Mental Health Act Apprehensions from https://open.toronto.ca/dataset/mental-health-apprehensions/
+  # Police Annual Statistical Report - Arrested and Charged Persons from https://open.toronto.ca/dataset/police-annual-statistical-report-arrested-and-charged-persons/
+
+#### Workspace setup ####
+library(opendatatoronto)
+library(tidyverse)
+
+
+#### Download data ####
+
+## Mental Health Act Apprehensions 
+mental_health_package <- search_packages("Mental Health Act Apprehensions")
+
+mental_health_resources <- 
+  mental_health_package %>%
+  list_package_resources()
+
+raw_mentalHealth_data <- 
+  mental_health_resources[4,] %>%
+  get_resource()
+
+
+## Police Annual Report
+package <- show_package("police-annual-statistical-report-arrested-and-charged-persons")
+
+# get all resources for this package
+resources <- list_package_resources("police-annual-statistical-report-arrested-and-charged-persons")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+raw_crime_data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+
+
+#### Save data ####
+# change the_raw_data to whatever name you assigned when you downloaded it.
+write.csv(raw_mentalHealth_data, "inputs/data/raw_mentalHealth_data.csv") 
+write.csv(raw_crime_data, "inputs/data/raw_crime_data.csv") 
+
+
+         
